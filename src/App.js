9aa9387b7aebd76f1d02b9b03d723ff24c44cc4c
Abideen <Abideenali926@gmail.com>
2022-04-10 23:44:01 -0700

    random choice pickers

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  Choicepiker from './compnents/Choicepiker'
+
+function App() {
+  return (
+    <>
+      <Choicepiker />
+    </>
+  );
+}
+
+export default App;
